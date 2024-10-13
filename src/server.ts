ab11757c04f665ec779b7a492c084705c1d6db88
devrick <lhpsystems0376@gmail.com>
2024-10-13 09:29:45 -0300

    Deploy Projeto Pizzaria API curso

---
+++ src/server.ts
@@ -0,0 +1,40 @@
+import express, { Request, Response, NextFunction } from 'express'
+import 'express-async-errors';
+import cors from 'cors';
+import path from 'path'
+
+import { router } from './routes'
+import fileUpload from 'express-fileupload'
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+//codigo para cloudinary
+app.use(fileUpload({
+  limits: { fileSize: 50 * 1024* 1024}  // No maximo de 50 mb
+}))
+
+app.use(router);
+
+app.use(
+  '/files',
+  express.static(path.resolve(__dirname, '..', 'tmp'))
+)
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  if(err instanceof Error){
+    //Se for uma instancia do tipo error
+    return res.status(400).json({
+      error: err.message
+    })
+  }
+
+  return res.status(500).json({
+    status: 'error',
+    message: 'Internal server error.'
+  })
+
+})
+
+//app.listen(3333, () => console.log('Servidor online!!!!'))
+app.listen(process.env.PORT, () => console.log('Servidor online!!!!'))
